f0f5c04eecc82199319a9442b975486af1fad849
Vue Lee <vuelee09@gmail.com>
2022-07-25 21:29:30 -0500

    add load data from localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,12 @@ $(".saveBtn").click(function () {
 //  check method: is before, is after
 // retrieve time block and check it against current time
 // save it: localStorage setitem to save things, getItem to retrieve things
+$("#900pm-hour .textarea").val(localStorage.getItem("900pm-hour"));
+$("#1000pm-hour .textarea").val(localStorage.getItem("1000pm-hour"));
+$("#1100pm-hour .textarea").val(localStorage.getItem("1100pm-hour"));
+$("#1200pm-hour .textarea").val(localStorage.getItem("1200pm-hour"));
+$("#1300pm-hour .textarea").val(localStorage.getItem("1300pm-hour"));
+$("#1400pm-hour .textarea").val(localStorage.getItem("1400pm-hour"));
+$("#1500pm-hour .textarea").val(localStorage.getItem("1500pm-hour"));
+$("#1600pm-hour .textarea").val(localStorage.getItem("1600pm-hour"));
+$("#1700pm-hour .textarea").val(localStorage.getItem("1700pm-hour"));
