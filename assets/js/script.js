578036d0d0c915b903aab77cb19dc770329fae9f
Vue Lee <vuelee09@gmail.com>
2022-07-25 23:26:23 -0500

    add function to audit the hour with color changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,68 @@
-const todaysDate = moment().format("dddd, MMMM Do YYYY"); {
-    document.getElementById("currentDay").innerHTML = todaysDate;
-};
-
-
-//  add event listener to target button for save textarea content value
-$(".saveBtn").click(function () {
-    let time = $(this).parent().attr("id");
-    let value = $(this).siblings(".textarea").val();
-    localStorage.setItem(time, value);
-})
-
+$(document).ready(function() {
+    // listen for save button clicks
+    $(".saveBtn").on("click", function() {
+   
+        let value = $(this).siblings(".textarea").val();
+        let key = $(this).parent().attr("id");
     
-// use moment to get current time, converting it to military time, 
-//  check method: is before, is after
-// retrieve time block and check it against current time
-// save it: localStorage setitem to save things, getItem to retrieve things
-$("#900pm-hour .textarea").val(localStorage.getItem("900pm-hour"));
-$("#1000pm-hour .textarea").val(localStorage.getItem("1000pm-hour"));
-$("#1100pm-hour .textarea").val(localStorage.getItem("1100pm-hour"));
-$("#1200pm-hour .textarea").val(localStorage.getItem("1200pm-hour"));
-$("#1300pm-hour .textarea").val(localStorage.getItem("1300pm-hour"));
-$("#1400pm-hour .textarea").val(localStorage.getItem("1400pm-hour"));
-$("#1500pm-hour .textarea").val(localStorage.getItem("1500pm-hour"));
-$("#1600pm-hour .textarea").val(localStorage.getItem("1600pm-hour"));
-$("#1700pm-hour .textarea").val(localStorage.getItem("1700pm-hour"));
+        // save in localStorage
+        localStorage.setItem(key, value);
+    
+      
+        $(".alert").addClass("show");
+    
+
+        setTimeout(function() {
+            $(".alert").removeClass("show");
+        }, 5000);
+    });
+  
+    function hourUpdater() {
+
+        let currentTime = moment().hours();
+
+        $(".hourly-plan").each(function() {
+            let time = parseInt(
+            $(this)
+                .attr("id")
+                .split("-")[1]
+            );
+    
+              if (time < currentTime) {
+                $(this).addClass("past");
+            } 
+            else if (time === currentTime) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            } 
+            else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        });
+    }
+  
+    hourUpdater();
+  
+    // set up interval to check if current time needs to be updated
+    let interval = setInterval(hourUpdater, 15000);
+
+
+    $("#900pm-hour .textarea").val(localStorage.getItem("900pm-hour"));
+    $("#1000pm-hour .textarea").val(localStorage.getItem("1000pm-hour"));
+    $("#1100pm-hour .textarea").val(localStorage.getItem("1100pm-hour"));
+    $("#1200pm-hour .textarea").val(localStorage.getItem("1200pm-hour"));
+    $("#1300pm-hour .textarea").val(localStorage.getItem("1300pm-hour"));
+    $("#1400pm-hour .textarea").val(localStorage.getItem("1400pm-hour"));
+    $("#1500pm-hour .textarea").val(localStorage.getItem("1500pm-hour"));
+    $("#1600pm-hour .textarea").val(localStorage.getItem("1600pm-hour"));
+    $("#1700pm-hour .textarea").val(localStorage.getItem("1700pm-hour"));
+    $("#2200pm-hour .textarea").val(localStorage.getItem("2200pm-hour"));
+    $("#2300pm-hour .textarea").val(localStorage.getItem("2300pm-hour"));
+
+
+    const todaysDate = moment().format("dddd, MMMM Do YYYY"); {
+        document.getElementById("currentDay").innerHTML = todaysDate;
+    };
+})
